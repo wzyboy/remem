98b58153b45deab276c7f13038dabb8502b7456b
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-03-28 15:17:37 -0700

    feat: ingest Telegram history dump

---
+++ remem/ingest/utils.py
@@ -0,0 +1,14 @@
+import itertools
+from pathlib import Path
+from collections.abc import Iterable
+
+
+def iter_files(paths: Iterable[Path], suffix: str) -> Iterable[Path]:
+    '''Find all files with the given suffix in paths recursively'''
+    def gen():
+        for path in paths:
+            if path.is_dir():
+                yield (p for p in path.rglob(f'*{suffix}') if p.is_file())
+            elif path.suffix == suffix:
+                yield [path]
+    yield from itertools.chain.from_iterable(gen())
